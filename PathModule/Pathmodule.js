584dc4d98f87bb32802b1abce82c8ad3b230dae1
shivang <18ec.shivangthacker@gmail.com>
2023-06-22 11:46:04 +0530

    path MOdule

--- PathModule/Pathmodule.js
+++ PathModule/Pathmodule.js
@@ -1,3 +1,11 @@
 const path = require("path");
 
-console.log(path);
+console.log(path.dirname("G:/nodejs/basicnode/PathModule/Pathmodule.js")); //shows whole path
+console.log(path.extname("G:/nodejs/basicnode/PathModule/Pathmodule.js")); //file extention
+console.log(path.basename("G:/nodejs/basicnode/PathModule/Pathmodule.js")); //filename
+
+console.log(path.parse("G:/nodejs/basicnode/PathModule/Pathmodule.js")); //it sjows root,dir,base,ext,name
+
+const Mypath = path.parse("G:/nodejs/basicnode/PathModule/Pathmodule.js"); //it sjows root,dir,base,ext,name
+console.log(Mypath.name); //shows only name
+console.log(Mypath.root); //shows only root
