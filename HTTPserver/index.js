f9bbce5d41b6ce7f3a0161a8585715eb90329841
shivang <18ec.shivangthacker@gmail.com>
2023-06-22 15:46:00 +0530

    create urls and messages

--- HTTPserver/index.js
+++ HTTPserver/index.js
@@ -2,7 +2,15 @@ const http = require("http"); //call http module
 
 //make server with createserver
 const server = http.createServer((req, res) => {
-  res.end("Hello from the other sides");
+  //console.log(req.url);(to show which url is called, like /about , /contact)
+  if (req.url == "/") {
+    res.end("Hello! welcome to homepage");
+  } else if (req.url == "/about") {
+    res.end("Hello! welcome to about page");
+  } else {
+    res.writeHead(404, { Contenttype: "text/html" });
+    res.end("<h1>This page is not exist</h1>"); //we can use HTML also
+  }
 });
 
 //to listen request or varify request we will use listen, we will use port,localhost
