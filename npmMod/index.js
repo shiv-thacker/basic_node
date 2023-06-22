d4d748d3b4c1056ce4d07e6f52d33210670c9a90
shivang <18ec.shivangthacker@gmail.com>
2023-06-22 12:52:21 +0530

    chalk and validator

---
+++ npmMod/index.js
@@ -0,0 +1,10 @@
+const chalk = require("chalk");
+const validator = require("validator");
+
+// console.log(chalk.blue("Hello world!"));
+// console.log(chalk.blue.italic("Hello world!"));
+// console.log(chalk.blue.underline.inverse("Hello world!"));
+// console.log(chalk.green.underline.inverse("Succeed"));
+
+const res = validator.isEmail("shivang@gmail.com");
+console.log(res ? chalk.green.inverse(res) : chalk.red.inverse(res));
