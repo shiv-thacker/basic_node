db8bbcf43657205eec3b06877e742894f8d06727
shivang <18ec.shivangthacker@gmail.com>
2023-06-22 20:00:21 +0530

    how to make api and fetch data from api

--- HTTPserver/index.js
+++ HTTPserver/index.js
@@ -1,4 +1,9 @@
 const http = require("http"); //call http module
+const fs = require("fs");
+
+//we will use Sync to read json because we don't want to call everytime
+const getdata = fs.readFileSync(`${__dirname}/UserApi/userapi.json`, "utf-8");
+const object = JSON.parse(getdata);
 
 //make server with createserver
 const server = http.createServer((req, res) => {
@@ -7,6 +12,9 @@ const server = http.createServer((req, res) => {
     res.end("Hello! welcome to homepage");
   } else if (req.url == "/about") {
     res.end("Hello! welcome to about page");
+  } else if (req.url == "/userapi.json") {
+    res.writeHead(200, { Contenttype: "application/json" }); //It is require for json file
+    res.end(object[1].name);
   } else {
     res.writeHead(404, { Contenttype: "text/html" });
     res.end("<h1>This page is not exist</h1>"); //we can use HTML also
