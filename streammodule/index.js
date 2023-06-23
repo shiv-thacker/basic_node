38ed2542937da51fa77e6651674ae58dd8d520a3
shivang <18ec.shivangthacker@gmail.com>
2023-06-23 12:00:30 +0530

    streaming with pipe and other ways

--- streammodule/index.js
+++ streammodule/index.js
@@ -1,22 +1,42 @@
 const http = require("http"); //call http module
 const fs = require("fs");
 
-// lwt's read without stream
+//  <---------------------------- 1st way----------------------------------------->
+
+// const server = http.createServer();
+
+// server.on("request", (req, res) => {
+//   fs.readFile("input.txt", (err, data) => {
+//     if (err) return console.log(err);
+//     res.end(data.toString());
+//   });
+// });
+
+//  <---------------------------- 2nd way----------------------------------------->
+// const server = http.createServer();
+// server.on("request", (req, res) => {
+//   const readstream = fs.createReadStream("input.txt");
+
+//   //chunkfiles is store data that we read
+//   readstream.on("data", (chunkfiles) => {
+//     res.write(chunkfiles);
+//   });
+//   readstream.on("end", () => {
+//     res.end();
+//   });
+//   readstream.on("error", (err) => {
+//     console.log(err);
+//     res.end("This file not found");
+//   });
+// });
+
+//  <---------------------------- 3rd way with pipe----------------------------------------->
+
 const server = http.createServer();
+
 server.on("request", (req, res) => {
   const readstream = fs.createReadStream("input.txt");
-
-  //chunkfiles is store data that we read
-  readstream.on("data", (chunkfiles) => {
-    res.write(chunkfiles);
-  });
-  readstream.on("end", () => {
-    res.end();
-  });
-  readstream.on("error", (err) => {
-    console.log(err);
-    res.end("This file not found");
-  });
+  readstream.pipe(res);
 });
 
 server.listen(8000, "127.0.0.1", () => {
