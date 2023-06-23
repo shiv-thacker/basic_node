1a9776fb9ac55e531bb6a0603104d9eff3a016b0
shivang <18ec.shivangthacker@gmail.com>
2023-06-23 11:47:18 +0530

    streaming files

---
+++ streammodule/index.js
@@ -0,0 +1,24 @@
+const http = require("http"); //call http module
+const fs = require("fs");
+
+// lwt's read without stream
+const server = http.createServer();
+server.on("request", (req, res) => {
+  const readstream = fs.createReadStream("input.txt");
+
+  //chunkfiles is store data that we read
+  readstream.on("data", (chunkfiles) => {
+    res.write(chunkfiles);
+  });
+  readstream.on("end", () => {
+    res.end();
+  });
+  readstream.on("error", (err) => {
+    console.log(err);
+    res.end("This file not found");
+  });
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to the port no 8000");
+});
